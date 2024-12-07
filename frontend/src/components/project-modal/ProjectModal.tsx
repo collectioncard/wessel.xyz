0a739de88d56c2f19837658c53cc46a3df49431f
Thomas Wessel <turpleturtle12@gmail.com>
2024-12-07 00:56:23 -0800

    Add initial image support to project modal

--- frontend/src/components/project-modal/ProjectModal.tsx
+++ frontend/src/components/project-modal/ProjectModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Modal from 'react-modal';
 import { Project } from '../../scripts/ProjectData';
 import './ProjectModal.css';
+import { Carousel } from 'nuka-carousel';
 
 interface ProjectModalProps {
     isOpen: boolean;
@@ -14,14 +15,16 @@ Modal.setAppElement('#root');
 const ProjectModal: React.FC<ProjectModalProps> = ({ isOpen, onRequestClose, project }) => {
     if (!project) return null;
 
-    //generate a string of every author in the authors array
-    let authors2 = 'Created by: ';
-    project.authors.forEach((author, index) => {
-        authors2 += author.name;
-        if (index !== project.authors.length - 1) {
-            authors2 += ', ';
-        }
-    });
+    const authors = project.authors.map((author, index) => (
+        <span key={index}>
+            {author.url ? (
+                <a href={author.url} target="_blank" rel="noopener noreferrer">{author.name}</a>
+            ) : (
+                author.name
+            )}
+            {index !== project.authors.length - 1 && ', '}
+        </span>
+    ));
 
     return (
         <Modal
@@ -33,7 +36,12 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ isOpen, onRequestClose, pro
         >
             <h1>{project.title}</h1>
             <h3>{project.subtitle}</h3>
-            <h6>{authors2}</h6>
+            <h6>Created by: {authors}</h6>
+            <Carousel showArrows showDots autoplay={true}>
+                {project.images.map((image, index) => (
+                    <img key={index} src={image.startsWith("http") ? image : `images/${project.assetFolder}/${image}`} alt={`Slide ${index}`} />
+                ))}
+            </Carousel>
             <p>{project.page_body}</p>
             <button onClick={onRequestClose}>Close</button>
         </Modal>
