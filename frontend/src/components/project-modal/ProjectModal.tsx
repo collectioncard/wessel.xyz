05cd75523c1ed87379d0bcd45e9ebff34b058ba8
Thomas Wessel <turpleturtle12@gmail.com>
2024-12-07 22:44:35 -0800

    Switch carousel lib
    Also disable scrolling the background page

--- frontend/src/components/project-modal/ProjectModal.tsx
+++ frontend/src/components/project-modal/ProjectModal.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Modal from 'react-modal';
 import { Project } from '../../scripts/ProjectData';
 import './ProjectModal.css';
-import { Carousel } from 'nuka-carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 interface ProjectModalProps {
     isOpen: boolean;
@@ -12,13 +13,30 @@ interface ProjectModalProps {
 
 Modal.setAppElement('#root');
 
+const responsive = {
+    all: {
+        breakpoint: { max: 4000, min: 0 },
+        items: 1
+    }
+};
+
 const ProjectModal: React.FC<ProjectModalProps> = ({ isOpen, onRequestClose, project }) => {
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [isOpen]);
+
     if (!project) return null;
 
     const authors = project.authors.map((author, index) => (
         <span key={index}>
             {author.url ? (
-                <a href={author.url} target="_blank" rel="noopener noreferrer">{author.name}</a>
+                <a href={author.url} target="_blank" rel="noopener noreferrer">
+                    {author.name}
+                </a>
             ) : (
                 author.name
             )}
@@ -37,11 +55,27 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ isOpen, onRequestClose, pro
             <h1>{project.title}</h1>
             <h3>{project.subtitle}</h3>
             <h6>Created by: {authors}</h6>
-            <Carousel showArrows showDots autoplay={true}>
-                {project.images.map((image, index) => (
-                    <img key={index} src={image.startsWith("http") ? image : `images/${project.assetFolder}/${image}`} alt={`Slide ${index}`} />
-                ))}
-            </Carousel>
+            <div className="carouselContainer">
+                <Carousel
+                    responsive={responsive}
+                    infinite={true}
+                    autoPlay={false}
+                    showDots={true}
+                    arrows={true}
+                >
+                    {project.images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={
+                                image.startsWith("http")
+                                    ? image
+                                    : `images/${project.assetFolder}/${image}`
+                            }
+                            alt={`Slide ${index}`}
+                        />
+                    ))}
+                </Carousel>
+            </div>
             <p>{project.page_body}</p>
             <button onClick={onRequestClose}>Close</button>
         </Modal>
